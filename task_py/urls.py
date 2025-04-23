a0a85ef6fc92f4c699676ac855d8564e5438deba
severedseikyo <thanmaysadguru5912@gmail.com>
2025-04-23 23:36:02 +0530

    Task Workshop/5

--- task_py/urls.py
+++ task_py/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from myapp import views
 
 urlpatterns = [
@@ -26,4 +27,9 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name="Dashboard"),
     path('logout/',views.user_logout,name="Logout"),
     path('create-post/',views.create_post,name="Create-Post"),
+    path('reset_password/',auth_views.PasswordResetView.as_view(),name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+
 ]
