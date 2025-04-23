8963b1229573c72bfc988343cb11b3da1d51cc6f
severedseikyo <thanmaysadguru5912@gmail.com>
2025-04-23 12:24:14 +0530

    Task Django Workshop/3 Added Post Table

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+      author=models.ForeignKey(User,on_delete=models.CASCADE)
+      title=models.CharField(max_length=150)
+      subtitle=models.CharField(max_length=250)
+      content=models.TextField()
+      published_on=models.DateTimeField(auto_now_add=True)
