b2d1dda3d7dd6aa9c7684873bd9602c05d6dd796
severedseikyo <thanmaysadguru5912@gmail.com>
2025-04-24 09:20:24 +0530

    Added custom reset templates

--- task_py/urls.py
+++ task_py/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name="Dashboard"),
     path('logout/',views.user_logout,name="Logout"),
     path('create-post/',views.create_post,name="Create-Post"),
-    path('reset_password/',auth_views.PasswordResetView.as_view(),name="reset_password"),
-    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='reset-password.html'),name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='reset-password-sent.html'),name="password_reset_done"),
+    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='password-reset-confirm.html'), name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password-reset-complete.html'),name="password_reset_complete"),
 
 ]
